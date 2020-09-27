9f9033051c00980ee9dbc5d78e7147b5e61af1d0
bzhuang8282 <41022000+bzhuang8282@users.noreply.github.com>
2020-09-26 22:24:48 -0400

    #10 improved file structure, added routes for pages, created folder for static files "static" for express server to use

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const path = require('path');
+const { response } = require('express');
 const port = 3000;
 
 const app = express();
 app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static('static'))
 
 app.get('/',function(req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, '/static/html/index.html'));
+});
+
+app.get('/search',function(req, res) {
+    res.sendFile(path.join(__dirname, '/static/html/searchpage.html'));
+});
+
+app.get('/FailedSearch',function(req, res) {
+    res.sendFile(path.join(__dirname, '/static/html/FailedSearch.html'));
 });
 
 
