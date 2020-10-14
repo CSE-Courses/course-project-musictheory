237c66cd8461dcc75de7035983dbd754c6a419f3
ethandoy <ethandoy@buffalo.edu>
2020-10-14 14:37:32 -0400

    Issue #29 Create genre dropdown section on search page to allow for distinguished search by genre

--- searchpage.js
+++ searchpage.js
@@ -2,13 +2,10 @@ $(document).ready(function(){
   $('.header').height($(window).height());
 })
 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
