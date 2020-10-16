3a6f9ca5f9cab564bce9f45f8a2a0280a16648a5
samraatg <samraatg@buffalo.edu>
2020-10-16 19:30:27 -0400

    Added basic Mongo db database

--- app.js
+++ app.js
@@ -9,6 +9,73 @@ app.use(express.static('static'))
 
 
 
+const mongoose = require("mongoose");
+const { stringify } = require('querystring');
+
+
+const uri = "mongodb+srv://musicTheory:GY1HZHC60eb2MKo5@cluster0.eg8k3.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('MongoDB Connected…')
+})
+.catch(err => console.log(err))
+
+
+var ArtistSchema =  new mongoose.Schema({
+    id : String,
+    name: String,
+    type : String,
+    uri: String 
+});
+var AlbumSchema = new mongoose.Schema({
+    name: String,
+    type : String,
+    artist: String,
+    id : String,
+    images : String, 
+    uri : String   
+
+});
+var TrackSchema = new mongoose.Schema({
+    artist: String,
+    duration: Number,
+    name : String,
+    id : String,
+    preview_url: String,
+    uri : String
+
+})
+ 
+var artist = mongoose.model("Artist", ArtistSchema);    // Adding to Database
+artist.create({
+    id : "1",
+    name: "Slipkot",
+    type : "Artist",
+    uri: "" 
+
+}, function(error,data){
+    if(error){
+        console.log("An error while adding to Collection");
+        console.log(error);
+    }else{
+        console.log("Data added sucessfully");
+        console.log(data);
+    }
+});
+
+artist.find({}, function(error,data){     //Retrieving data
+    if(error){
+        console.log("problem finding data");
+    }else{
+        console.log("Here is all the data");
+        console.log(data);
+    }
+});
+
+
 
 
 
