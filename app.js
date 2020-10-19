e43d84ff0e6394473973d487f35c664e40995797
david-cam <davidcam@buffalo.edu>
2020-10-19 04:05:42 -0400

    procfile,made changes to package.json

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { response } = require('express');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.static(path.join(__dirname, 'client')));
